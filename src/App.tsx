250a6a6f87de5f13d4735a5d653ceec58c0c4122
D4ryl3ne1 <Darylehen02@gmail.com>
2020-05-23 21:12:13 -0400

    defined todolist props using interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import TodoList from './components/TodoList'
 
 const App: React.FC = () => {
+  const todos = [{id: 't1', text: 'Finish reading the Glass Castle'}]
   return (
     <div className="App">
+      {/* A component that adds todos*/}
+      <TodoList items={todos} />
     </div>
   );
 }
