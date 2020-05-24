bc5154e87fb89c73f13b4153e21abeb814c7f8bd
D4ryl3ne1 <Darylehen02@gmail.com>
2020-05-24 02:06:01 -0400

    Implemented NewTodo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import TodoList from './components/TodoList'
+import TodoList from './components/TodoList';
+import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{id: 't1', text: 'Finish reading the Glass Castle'}]
   return (
     <div className="App">
+      <NewTodo />
       {/* A component that adds todos*/}
       <TodoList items={todos} />
     </div>
