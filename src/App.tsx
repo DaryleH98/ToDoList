e9f15f36be62ef01ad16549e45bba13168b819f9
D4ryl3ne1 <Darylehen02@gmail.com>
2020-05-24 03:23:56 -0400

    implemented delete handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Todo } from './todo.model';
+
 import TodoList from './components/TodoList';
 import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
-  const todos = [{id: 't1', text: 'Finish reading the Glass Castle'}]
+  const [todos, setTodos]= useState<Todo[]>([]);
 
   const todoAddHandler = (text: string) => {
-      console.log(text);
+      setTodos(prevTodos => [...prevTodos,
+      {id: Math.random().toString(), text: text}]);
   };
   
+  const todoDeleteHandler = (todoId: string) => {
+    setTodos(prevTodos => {
+      return prevTodos.filter(todo => todo.id !== todoId);
+    });
+  };
   return (
     <div className="App">
       <NewTodo onAddTodo={todoAddHandler} />
       {/* A component that adds todos*/}
-      <TodoList items={todos} />
+      <TodoList items={todos} onDeleteTodo={todoDeleteHandler}/>
     </div>
   );
 }
