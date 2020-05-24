9b7f3d2e7c5c267607faaae6f519bcd6dce816de
D4ryl3ne1 <Darylehen02@gmail.com>
2020-05-24 02:41:17 -0400

    created type definition for NewToDoProps

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,14 @@ import NewTodo from './components/NewTodo';
 
 const App: React.FC = () => {
   const todos = [{id: 't1', text: 'Finish reading the Glass Castle'}]
+
+  const todoAddHandler = (text: string) => {
+      console.log(text);
+  };
+  
   return (
     <div className="App">
-      <NewTodo />
+      <NewTodo onAddTodo={todoAddHandler} />
       {/* A component that adds todos*/}
       <TodoList items={todos} />
     </div>
